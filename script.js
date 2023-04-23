4c33cc4cab78891158fbb34eccb16378df5f6810
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-23 16:39:26 -0600

    Add function to update play icon

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ function toggleVideoStatus() {
 }
 
 function updatePlayIcon() {
-  return true;
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
 }
 
 // Update progress & timestamp
