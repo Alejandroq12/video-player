2d576a3a574a67ec748eeb150ceb1661f2ad5e44
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-23 16:10:52 -0600

    Add event listeners

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
