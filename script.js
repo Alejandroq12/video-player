8881f6a055f65b8214490a0ebe840a3ab51c0f8e
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-23 16:08:21 -0600

    Get elements from the DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
