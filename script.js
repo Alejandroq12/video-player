009d08af84bcee84b0d383f9826f052ca5088493
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-23 18:43:27 -0600

    Add function to update progress bar

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video');
 const play = document.getElementById('play');
-const stop1 = document.getElementById('stop');
+const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
-// Play & Pause Video
+// Play & pause video
 function toggleVideoStatus() {
   if (video.paused) {
     video.play();
@@ -13,6 +13,7 @@ function toggleVideoStatus() {
   }
 }
 
+// update play/pause icon
 function updatePlayIcon() {
   if (video.paused) {
     play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
@@ -24,6 +25,20 @@ function updatePlayIcon() {
 // Update progress & timestamp
 function updateProgress() {
   progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
@@ -37,7 +52,7 @@ function stopVideo() {
   video.pause();
 }
 
-// Event Listeners
+// Event listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
@@ -45,6 +60,6 @@ video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
-stop1.addEventListener('click', stopVideo);
+stop.addEventListener('click', stopVideo);
 
 progress.addEventListener('change', setVideoProgress);
